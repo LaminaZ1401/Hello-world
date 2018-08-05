7f389c3ecfc00c84c12639332bdc932aae60ca12
LaminaZ1401 <36857122+LaminaZ1401@users.noreply.github.com>
2018-08-05 14:57:15 -0300

    Add files via upload

---
+++ Hello_world.java
@@ -0,0 +1,12 @@
+public class Hello_world {
+    public static void main(String[] args)
+    {
+        byte idade = 17;
+                System.out.println("A idade é " + idade);
+        char sexo = 'M';
+
+        boolean servirExercito = (idade >=18 && sexo == 'M');
+            System.out.println("Pode servir ao Exercito:" + servirExercito);
+
+    }
+}
